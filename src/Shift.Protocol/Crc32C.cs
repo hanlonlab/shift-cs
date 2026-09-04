d93eb19766e6770f4f5b5b4828d77ee9363e5a84
Marcus Mikelic <29722978+samoriental@users.noreply.github.com>
2026-09-04 16:08:52 -0400

    CRC32C speedup

--- src/Shift.Protocol/Crc32C.cs
+++ src/Shift.Protocol/Crc32C.cs
@@ -1,3 +1,4 @@
+using System.Buffers.Binary;
 using System.Numerics;
 
 namespace Shift.Protocol;
@@ -7,6 +8,12 @@ public static class Crc32C
     public static uint Compute(ReadOnlySpan<byte> source)
     {
         uint checksum = uint.MaxValue;
+        while (source.Length >= sizeof(ulong))
+        {
+            checksum = BitOperations.Crc32C(checksum, BinaryPrimitives.ReadUInt64LittleEndian(source));
+            source = source[sizeof(ulong)..];
+        }
+
         foreach (byte value in source)
         {
             checksum = BitOperations.Crc32C(checksum, value);
--- test/Shift.Protocol.Tests/Crc32CTests.cs
+++ test/Shift.Protocol.Tests/Crc32CTests.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Xunit;
 
 namespace Shift.Protocol.Tests;
@@ -10,5 +11,24 @@ public class Crc32CTests
         byte[] source = [0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39];
 
         Assert.Equal(0xe3069283u, Crc32C.Compute(source));
+    }
+
+    [Fact]
+    public void ComputeMatchesByteAtATimeForEveryRemainder()
+    {
+        byte[] source = new byte[65];
+        new Random(42).NextBytes(source);
+
+        for (int length = 0; length < source.Length; length++)
+        {
+            ReadOnlySpan<byte> data = source.AsSpan(1, length);
+            uint checksum = uint.MaxValue;
+            foreach (byte value in data)
+            {
+                checksum = BitOperations.Crc32C(checksum, value);
+            }
+
+            Assert.Equal(~checksum, Crc32C.Compute(data));
+        }
     }
 }
